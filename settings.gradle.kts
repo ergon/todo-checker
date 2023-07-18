2a86a2f2aa481e12bca61b71ea5424e90d03da17
Raphael Fuchs <raphael.fuchs@ergon.ch>
2023-07-18 08:56:06 +0200

    Use artifactory for maven central dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,9 @@ rootProject.name = "todo-checker"
 
 dependencyResolutionManagement {
 	repositories {
-		mavenCentral()
+		maven {
+			url = uri("https://artifacts.ergon.ch/artifactory/proxy-maven-central/")
+		}
 		maven {
 			url = uri("https://artifacts.ergon.ch/artifactory/proxy-atlassian/")
 		}
