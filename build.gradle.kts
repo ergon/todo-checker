c00b006843b7c4851f4159691221400963309c74
Raphael Fuchs <raphael.fuchs@ergon.ch>
2023-07-18 09:37:23 +0200

    Configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
 	`java-gradle-plugin`
+	`maven-publish`
 	alias(libs.plugins.kotlin.jvm)
 	alias(libs.plugins.detekt)
 	alias(libs.plugins.spotless)
 }
 
+val repositoryArtifactoryUsername: String by project
+val repositoryArtifactoryApiKey: String by project
+
 group = "ch.ergon.todochecker"
+version = project.findProperty("buildVersion") ?: "0.0.1-local"
 
 gradlePlugin {
 	plugins {
@@ -20,6 +25,19 @@ gradlePlugin {
 
 dependencies {
 	compileOnly(libs.jira.rest.java.client)
+}
+
+publishing {
+	repositories {
+		maven {
+			url = uri("https://artifacts.ergon.ch/artifactory/proj-ergon-cop-cd-releases/")
+			credentials {
+				username = repositoryArtifactoryUsername
+				password = repositoryArtifactoryApiKey
+			}
+			authentication { create<BasicAuthentication>("basic") }
+		}
+	}
 }
 
 dependencyLocking {
