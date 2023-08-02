a563dbf2977c54a6f5b09fac2178692b2d82657b
Phokham Nonava <phokham.nonava@ergon.ch>
2023-08-02 17:20:25 +0200

    Revert "Remove Serializable"
    This reverts commit 67a7f52e92325b43f844c3b4f0a88c95cb8acf6b.

--- src/main/kotlin/ch/ergon/todochecker/JiraCredentials.kt
+++ src/main/kotlin/ch/ergon/todochecker/JiraCredentials.kt
@@ -4,7 +4,17 @@
 
 package ch.ergon.todochecker
 
+import java.io.Serializable
+
 sealed interface JiraCredentials {
-	data class UsernamePassword(val username: String, val password: String) : JiraCredentials
-	data class PersonalAccessToken(val token: String) : JiraCredentials
+	data class UsernamePassword(val username: String, val password: String) : JiraCredentials, Serializable {
+		companion object {
+			private const val serialVersionUID: Long = 1
+		}
+	}
+	data class PersonalAccessToken(val token: String) : JiraCredentials, Serializable {
+		companion object {
+			private const val serialVersionUID: Long = 1
+		}
+	}
 }
