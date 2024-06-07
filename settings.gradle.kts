e78740924f7689f5a85180a58d80955642aadb6a
Phokham Nonava <phokham.nonava@ergon.ch>
2024-06-07 17:17:52 +0200

    Add sources

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "todo-checker"
+
+dependencyResolutionManagement {
+	repositories {
+		mavenCentral()
+		maven {
+			url = uri("https://packages.atlassian.com/mvn/maven-external/")
+		}
+	}
+}
